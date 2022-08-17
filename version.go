0b0bbf0f9c0fc187299ed44cc5c627a0c18f8e2f
Eric Duong <eeoneric@gmail.com>
2022-08-17 10:26:04 -0400

    Update version and add changelog (#7)

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package posthog
 import "flag"
 
 // Version of the client.
-const Version = "1.0.2"
+const Version = "2.0.0"
 
 // make tests easier by using a constant version
 func getVersion() string {
