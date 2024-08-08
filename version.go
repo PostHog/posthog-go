96410d9e51067c605a2474faae7a4c2cbb70cede
James Greenhill <fuziontech@gmail.com>
2024-08-07 17:03:17 -0700

    fix: change version from 2.0 to 1.2 (we are not on v2 yet) (#47)

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package posthog
 import "flag"
 
 // Version of the client.
-const Version = "2.0.0"
+const Version = "1.2.1"
 
 // make tests easier by using a constant version
 func getVersion() string {
